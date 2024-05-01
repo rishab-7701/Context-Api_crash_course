07b247e7f1d3464970df08f78721dbda1adaef70
rishab_7701 <agarwalrishab50@gmail.com>
2024-05-01 21:02:50 +0530

    Context Api learning

---
+++ mini_context/src/context/UserContext.js
@@ -0,0 +1,7 @@
+ // context is nothing but can be considered as a global variabl
+import React from "react";
+import { useContext } from "react";
+
+const UserContext = React.createContext()
+
+export default UserContext;
